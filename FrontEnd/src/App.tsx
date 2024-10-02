3848762a49d8d1f01f53f2a7d9811f206232b86d
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-02 16:55:37 -0300

    Configurando a rota principal.

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './routes/HomePage'
 
 function App() {
 
   return (
-    <HomePage />
+    <BrowserRouter>
+    <Routes>
+<Route path="/" element={<HomePage />}>
+</Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
